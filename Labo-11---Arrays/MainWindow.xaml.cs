585c39d55431f80e1640527f8a2fc5f277175a6e
Wannes Pipien <wannes.pipien@student.pxl.be>
2024-11-19 17:08:16 +0100

    labo commit

--- Labo-11---Arrays/MainWindow.xaml.cs
+++ Labo-11---Arrays/MainWindow.xaml.cs
@@ -20,5 +20,68 @@ namespace Labo_11___Arrays
         {
             InitializeComponent();
         }
+
+        StringBuilder sb = new StringBuilder();
+
+        private void outputButton_Click(object sender, RoutedEventArgs e)
+        {
+            int[] numbers = { 100, 50, 20, 60, 90, 80 };
+            int[] biggestAndSmallest = ReturnSmallestAndBiggest(numbers);
+            sb.AppendLine("-----------------------------");
+            sb.AppendLine($"Er zijn in totaal {numbers.Length} getallen.");
+            sb.AppendLine($"Het kleinste getal is: {biggestAndSmallest[0].ToString()}");
+            sb.AppendLine($"Het grootste getal is: {biggestAndSmallest[1].ToString()}");
+            OrderArray();
+        }
+
+        private int[] ReturnSmallestAndBiggest(int[] numbers)
+        {
+            int min = numbers[0];
+            int max = numbers[0];
+
+            for (int i = 1; i < numbers.Length; i++)
+            {
+                if (numbers[i] > max)
+                {
+                    max = numbers[i];
+                }
+                else if (numbers[i] < min)
+                {
+                    min = numbers[i];
+                }
+
+            }
+
+            int[] smallestAndBiggest = { min, max };
+            return smallestAndBiggest;
+        }
+
+        private void OrderArray()
+        {
+            int[] orderNumbers = { 100, 50, 20, 60, 90, 80 };
+            int numberSwitch;
+
+            for (int i = 0; i < orderNumbers.Length; i++)
+            {
+                for (int a = (1 + i); a < orderNumbers.Length; a++)
+                {
+                    if (orderNumbers[i] > orderNumbers[a])
+                    {
+                        numberSwitch = orderNumbers[i];
+                        orderNumbers[i] = orderNumbers[a];
+                        orderNumbers[a] = numberSwitch;
+                    }
+                }
+            }
+
+            foreach (int value in orderNumbers)
+            {
+                sb.AppendLine(value.ToString());
+            }
+
+            outputTextBox.Text = sb.ToString();
+        }
+
+
     }
 }
